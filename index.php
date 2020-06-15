20a4d528263dd8974f3bbf86e2a0d358773396e8
satwikagunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-06-15 14:53:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<h2>HTML Image</h2>
+<img src="pic_trulli.jpg"" alt="Trulli" width="500" height="333">
 <?php
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
