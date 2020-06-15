26d40cc7a0f200daaa405ff357b67ac95cdc385e
satwikagunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-06-15 13:48:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-welcome to lw cloud
+Enjoy Nature 
+Add colours to life
 <br />
 
-<pre>
-<?php
 
-print `ifconfig`;
+<?php
+$url="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw0xcCp_7PR4ih9--76Z1-bO&ust=1592295385458000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCID8xNiwg-oCFQAAAAAdAAAAABAT"
+$image=file_get_contents("$url");
+echo $image;
 
 ?>
-</pre>
+
