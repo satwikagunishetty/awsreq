712b99ed92338f4f951e6a150bf9c52b9dc5c91d
satwikagunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-06-15 14:33:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-Enjoy Nature 
-Add colours to life
-<br />
+<!DOCTYPE html>
+<html>
+<body>
 
-<pre>
 <?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
 
-echo satwika;
-echo image;
-
-
-?>
-</pre>
+</body>
+</html>
